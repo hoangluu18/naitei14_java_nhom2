95c2f95fbd0edbafc6d3dae0b489f7caa2111192
Lê Trần Diễm Ly <147296100+diemly26@users.noreply.github.com>
2025-11-24 09:00:44 +0700

    Implement task [95410]: Xây dựng các entities liên quan đến Skill (Skill, UserSkill) (#9)

--- src/main/java/vn/sun/membermanagementsystem/entities/Skill.java
+++ src/main/java/vn/sun/membermanagementsystem/entities/Skill.java
@@ -1,14 +1,34 @@
 package vn.sun.membermanagementsystem.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Data
 @Table(name = "skills")
 public class Skill {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @OneToMany(mappedBy = "skill", fetch = FetchType.LAZY)
+    private List<UserSkill> userSkills;
 }
--- src/main/java/vn/sun/membermanagementsystem/entities/UserSkill.java
+++ src/main/java/vn/sun/membermanagementsystem/entities/UserSkill.java
@@ -1,14 +1,56 @@
 package vn.sun.membermanagementsystem.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
-@Table(name = "user_skills")
+@Table(
+        name = "user_skills",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "idx_unique_user_skill",
+                        columnNames = {"user_id", "skill_id"}
+                )
+        }
+)
 public class UserSkill {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false,
+            foreignKey = @ForeignKey(name = "fk_us_user"))
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "skill_id", nullable = false,
+            foreignKey = @ForeignKey(name = "fk_us_skill"))
+    private Skill skill;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Level level;
+
+    public enum Level {
+        BEGINNER,
+        INTERMEDIATE,
+        ADVANCED,
+        EXPERT
+    }
+
+    @Column(name = "used_year_number", precision = 4, scale = 2)
+    private BigDecimal usedYearNumber;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
